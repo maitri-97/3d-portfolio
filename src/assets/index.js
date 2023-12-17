0513b67cc48f29587758ca06a235573756a16343
maitri-97 <137794833+maitri-97@users.noreply.github.com>
2023-12-17 22:45:45 +0530

    Add files via upload

---
+++ src/assets/index.js
@@ -0,0 +1,54 @@
+import logo from "./logo.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import bootstrap from "./tech/bootstrap.png";
+import jquery from "./tech/jquery.png";
+import reactjs from "./tech/reactjs.png";
+import sketch from "./tech/sketch.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+
+import tatvasoft from "./company/tatvasoft.png";
+import viitorcloud from "./company/viitorcloud.png";
+import freelancer from "./company/freelancer.png";
+
+import bookstore from "./bookstore.png";
+import blogapp from "./blogapp.png";
+import movieapp from "./movieapp.png";
+import portfolio from "./portfolio.png";
+
+export {
+  logo,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  figma,
+  sketch,
+  git,
+  html,
+  javascript,
+  bootstrap,
+  jquery,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  tatvasoft,
+  viitorcloud,
+  freelancer,
+  bookstore,
+  blogapp,
+  movieapp,
+  portfolio,
+};
